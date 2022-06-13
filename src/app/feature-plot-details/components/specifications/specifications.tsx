c443ae12fe666a8b1b62a674515d5acf134811d8
freelance <chandrashekar.develop@gmail.com>
2022-06-13 21:07:10 +0530

    Specifications and legal component

---
+++ src/app/feature-plot-details/components/specifications/specifications.tsx
@@ -0,0 +1,30 @@
+import { SpecificationsType } from 'app/feature-plot-details/models';
+import { Header } from '../header/header';
+import styles from './specifications.module.scss';
+export interface SpecificationsProps {
+  data: SpecificationsType[];
+}
+
+export const Specifications = ({ data }: SpecificationsProps) => {
+  return (
+    <div>
+      <Header title='Specifications' />
+      <section className={styles['spec-container']}>
+        {data.map((specification) => {
+          return (
+            <div className={styles['spec-deatils']} key={specification.label}>
+              <p>
+                <strong>{specification.label}</strong>
+              </p>
+              <ul>
+                {specification.data.map((label) => {
+                  return <li key={label}>{label}</li>;
+                })}
+              </ul>
+            </div>
+          );
+        })}
+      </section>
+    </div>
+  );
+};
